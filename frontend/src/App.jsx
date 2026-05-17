0c578930ea599cd62f90f8e76b62bcba174d862c
Priyanka Debnath <priyanka.neub@gmail.com>
2026-05-18 00:13:50 +0600

    3r commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import Contact from './pages/Contact'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -17,7 +18,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/doctors' element={<Doctors/>} />
-        <Route path='/doctor/:speciality' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/my-appointments' element={<MyAppointments/>} />
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>} />
         <Route path='/appointment' element={<Appointment/>} />
       </Routes>
+      <Footer/>
       
     </div>
   )
--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from './Header'
 import {specialityData} from '../assets/assets.js'
 import { Link } from 'react-router-dom'
 
