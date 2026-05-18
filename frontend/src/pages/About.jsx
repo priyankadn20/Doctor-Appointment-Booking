e6a5bd518dbd533e38c3049e38bff5efcd04f132
Priyanka Debnath <priyanka.neub@gmail.com>
2026-05-19 00:49:08 +0600

    4th commit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to Prescripto, your trusted platform for booking doctor appointments quickly and easily.</p>
+          <p>We connect you with the best doctors across all specialities. Book appointments effortlessly, manage your health records, and get the care you deserve — all in one place.</p>
+          <b className='text-gray-800'>Ouer Vision</b>
+          <p>Our vision at Prescripto is to make quality healthcare accessible to everyone. We strive to bridge the gap between patients and doctors, ensuring you receive the right care at the right time.</p>
+        </div>
+      </div>
+      <div className='text-xl my-4'>
+        <p>WHY <span>CHOOSE US</span></p>
+      </div>
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b> Efficiency:</b>
+          <p>Streamlined appointmnet scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience</b>
+          <p>Access to a network of trusted healthcare professinals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>personalization</b>
+          <p>Trailored recommandations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
     </div>
   )
 }
