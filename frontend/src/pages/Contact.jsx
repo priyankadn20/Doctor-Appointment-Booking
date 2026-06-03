9d0481f6c50ddd05b867cdff5edd9319b54463d8
Priyanka Debnath <priyanka.neub@gmail.com>
2026-06-03 13:57:29 +0600

    commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
+
+
   return (
     <div>
-      
+      <div className='text-xenter text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 fond semibold'>US</span></p>
+      </div>
+      <div>
+        <img src={assets.contact_image} />
+        <div>
+          <p>Our Office</p>
+          <p>3100,Noyasorok,Thikana <br/>tower,Sylhet,Bangladesh</p>
+          <p>Tel: (415) 555-0123 <br/>Email: info@company.com</p>
+          <p>Careers at PRESCRIPTO</p>
+          <p>Learn more about our teams and job openings</p>
+          <button>Explore jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
