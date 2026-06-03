1cb5bcd69a51d327eff149b5aea182be70b183ee
Priyanka Debnath <priyanka.neub@gmail.com>
2026-06-04 01:23:20 +0600

    commit

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,32 +1,32 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const MyProfile = () => {
 
-  const [userData,setUserData] = useState({
-    name:'preyanka debnath',
-    image:assets.profile_pic,
-    email:'preyankadebnath2002@gmail.com',
-    phone:'123-456-7890',
-    address:{
-      line1:'3100,Noyasorok,Thikana tower',
-      line2:'Sylhet,Bangladesh'
+  const [userData, setUserData] = useState({
+    name: 'preyanka debnath',
+    image: assets.profile_pic,
+    email: 'preyankadebnath2002@gmail.com',
+    phone: '123-456-7890',
+    address: {
+      line1: '3100,Noyasorok,Thikana tower',
+      line2: 'Sylhet,Bangladesh'
     },
-    gender:'Female',
-    dob:'2002-01-01'
-    
+    gender: 'Female',
+    dob: '2002-01-01'
+
   })
-  const [isEdit,setIsEdit] = useState(false)
+  const [isEdit, setIsEdit] = useState(true)
 
   return (
     <div>
-      <img src={userData.image} alt=""/>
+      <img src={userData.image} alt="" />
       {
         isEdit
-        ?<input type="text" value={userData.name} onChange={(e)=>setUserData(prev => ({...prev, name: e.target.value}))}/>
-        :<p>{userData.name}</p>
+          ? <input type="text" value={userData.name} onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} />
+          : <p>{userData.name}</p>
       }
-      <hr/>
+      <hr />
       <div>
         <p>CONTACT INFORMATION</p>
         <div>
@@ -34,12 +34,54 @@ const MyProfile = () => {
           <p>{userData.email}</p>
           <p>Phone:</p>
           {
-             isEdit
-            ?<input type="text" value={userData.phone} onChange={(e)=>setUserData(prev => ({...prev, phone: e.target.value}))}/>
-            :<p>{userData.phone}</p>
+            isEdit
+              ? <input type="text" value={userData.phone} onChange={(e) => setUserData(prev => ({ ...prev, phone: e.target.value }))} />
+              : <p>{userData.phone}</p>
           }
-         
+          <p>Address:</p>
+          {
+            isEdit
+            ? <p>
+              <input onChange={(e) => setUserData(prev=> ({...prev,address:{...prev.address, line1:e.target.value}}))} value={userData.address.line1} type="text" />
+              <br />
+              <input onChange={(e) => setUserData(prev=> ({...prev,address:{...prev.address, line2:e.target.value}}))} value={userData.address.line2} type="text" />
+            </p>
+            :<p>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          }
+
         </div>
+      </div>
+      <div>
+        <p>BASIC INFORMATION</p>
+        <div>
+          <p>Gender:</p>
+          {
+            isEdit
+              ? <select value={userData.gender} onChange={(e) => setUserData(prev => ({ ...prev, gender: e.target.value }))}>
+                <option value="Male">Male</option>
+                <option value="Female">Female </option>
+                <option value="Other">Other</option>
+              </select>
+              : <p>{userData.gender}</p>
+          }
+          <p>BirthDay:</p>
+          {
+            isEdit
+              ? <input type="date" value={userData.dob} onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} />
+              : <p>{userData.dob}</p>
+          }
+        </div>
+      </div>
+      <div>
+        {
+          isEdit            
+            ? <button onClick={() => setIsEdit(false)}>Save information</button>
+            : <button onClick={() => setIsEdit(true)}>Edit</button>
+        }
       </div>
     </div>
   )
